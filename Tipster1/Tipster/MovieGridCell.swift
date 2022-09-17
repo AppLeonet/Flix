63160999bebf3c540605478e869dca54beabb976
AppLeonet <hozhengmandeng@gmail.com>
2022-09-16 20:07:42 -0400

    updated

---
+++ Tipster1/Tipster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Man deng Ho on 9/16/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
